6aed198739e311860309b032406f083e846772bd
sean <yepaik@calpoly.edu>
2025-10-10 14:51:36 -0700

    task1 pt 3 almost completed

--- task1.py
+++ task1.py
@@ -1,3 +1,4 @@
+import secrets
 from Crypto.Cipher import AES
 import hashlib
 import random
@@ -7,6 +8,8 @@ from Crypto.Util.Padding import pad
 
 def main():
     input1 = input("Enter input: ")
+    print()
+    print("Task 1 & 2")
     input1 = input1.encode()
 
     #bit flipping
@@ -22,6 +25,23 @@ def main():
     print("Input hashed: " , input_hash)
     input_bitflipped_hash = hashlib.sha256(input1_bitflipped).digest()
     print("Input bitflipped hashed: " , input_bitflipped_hash)
+    print('\n\n')
+
+    input_hash_truncated = input_hash[:1]
+    random_hash = input_bitflipped_hash[:1]
+
+    i = 0
+    while(input_hash_truncated != random_hash):
+        random_hash = secrets.token_bytes(1) #random 1 byte string
+        print("Attempt ", i , " unsuccessful")
+        i += 1
+
+    print("Attempt ", i, " successful\n")
+    
+    random_hash = random_hash.hex()
+    print("Word is: ", random_hash)
+
+
 
 
 
