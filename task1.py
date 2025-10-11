8655552f57439efb170d5ccdb9750ea3248f4ae5
sean <yepaik@calpoly.edu>
2025-10-11 13:04:23 -0700

    changed from bruteforce to birthday attack

--- task1.py
+++ task1.py
@@ -2,9 +2,29 @@ import secrets
 from Crypto.Cipher import AES
 import hashlib
 import random
-from hashlib import sha256
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad
+import time
+
+def findCollision(size): 
+    #i = 0
+    #random_hash = secrets.randbits(size)
+    #while(target != random_hash):
+        #random_hash = secrets.randbits(size) #random [size] bits 
+        #print("Attempt ", i , " unsuccessful")
+        #i += 1
+    
+    #print("Attempt ", i, " successful\n")
+    #random_hash = hex(random_hash)
+    #return(random_hash)
+
+    seen = {}
+    while True:
+        random_bytes = secrets.token_bytes(9)
+        random_bytes = hashlib.sha256(random_bytes).digest()
+        random_bytes = int.from_bytes(random_bytes, 'big') >> (256 - size)
+        if random_bytes in seen:
+            return (seen[random_bytes], random_bytes)  
+        seen[random_bytes] = random_bytes
+
 
 def main():
     input1 = input("Enter input: ")
@@ -27,20 +47,13 @@ def main():
     print("Input bitflipped hashed: " , input_bitflipped_hash)
     print('\n\n')
 
-    input_hash_truncated = input_hash[:1]
-    random_hash = input_bitflipped_hash[:1]
 
-    i = 0
-    while(input_hash_truncated != random_hash):
-        random_hash = secrets.token_bytes(1) #random 1 byte string
-        print("Attempt ", i , " unsuccessful")
-        i += 1
-
-    print("Attempt ", i, " successful\n")
-    
-    random_hash = random_hash.hex()
-    print("Word is: ", random_hash)
-
+    for bits in range(8, 52, 2):
+        start = time.time()
+        findCollision(bits)  
+        end = time.time()
+        final_time = end - start
+        print("Bit Size: " , bits, " Time took: " , final_time)
 
 
 
